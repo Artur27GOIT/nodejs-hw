7a043c5663c20ab3481480285705df9a2d9cd29f
Artur27 <arturfurman7@gmail.com>
2026-05-01 09:38:45 +0200

    fix error 1

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import pino from "pino-http";
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const pinoHttp = require("pino-http");
 
 dotenv.config();
 
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(pino());
+app.use(pinoHttp());
 
 app.get("/notes", (req, res) => {
   res.status(200).json({ message: "Retrieved all notes" });
