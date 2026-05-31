986feb9e25f50db0429f820ab89dda2300de8eed
Artur27 <arturfurman7@gmail.com>
2026-05-31 12:49:19 +0200

    first time dp

---
+++ src/utils/saveFileToCloudinary.js
@@ -0,0 +1,24 @@
+import { v2 as cloudinary } from "cloudinary";
+import { Readable } from "stream";
+
+const { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } = process.env;
+
+cloudinary.config({
+  cloud_name: CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
+});
+
+export const saveFileToCloudinary = (buffer) => {
+  return new Promise((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream({ folder: "avatars" }, (error, result) => {
+      if (error) return reject(error);
+      resolve(result);
+    });
+
+    const readable = new Readable();
+    readable.push(buffer);
+    readable.push(null);
+    readable.pipe(stream);
+  });
+};
